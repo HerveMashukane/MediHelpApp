703695592c8cd208cf8221cfec315e0ff76688e0
Herve <hervemashukane@gmail.com>
2025-08-26 22:31:05 +0100

    rebuilt app architecture

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,10 @@
+import { NavbarComponent } from './layout/navbar/navbar.component';
+import { SidebarComponent } from './layout/sidebar/sidebar.component';
+import { FooterComponent } from './layout/footer/footer.component'
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path: '', component: NavbarComponent},
+    {path: '', component: SidebarComponent},
+    {path: '', component: FooterComponent},
+];
