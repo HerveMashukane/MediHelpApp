d0a4a35b1bf8e91e5156960db02f32ad4b4fbc7d
Herve <hervemashukane@gmail.com>
2025-08-28 14:00:55 +0100

    restructure sidebar template for better semantic and UX best practices

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,10 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { NavbarComponent } from './layout/navbar/navbar.component';
-import { SidebarComponent } from './layout/sidebar/sidebar.component';
-import { FooterComponent } from './layout/footer/footer.component';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, NavbarComponent, SidebarComponent, FooterComponent],
+  imports: [RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
---
+++ src/app/pages/dashboard/widgets/notification-summary/notification-summary.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-notification-summary',
+  standalone: true,
+  imports: [],
+  templateUrl: './notification-summary.component.html',
+  styleUrl: './notification-summary.component.css'
+})
+export class NotificationSummaryComponent {
+
+}
