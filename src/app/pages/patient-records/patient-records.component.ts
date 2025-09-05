083f4c86f6f6a6b6fae727bd49ad7fa45dd18b47
Herve <hervemashukane@gmail.com>
2025-09-05 10:59:38 +0100

    build and style patient records page

--- src/app/pages/patient-records/patient-records.component.ts
+++ src/app/pages/patient-records/patient-records.component.ts
@@ -1,12 +1,68 @@
+import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-patient-records',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './patient-records.component.html',
   styleUrl: './patient-records.component.css'
 })
 export class PatientRecordsComponent {
-
+  patients: any = [
+    {
+      preferedName: 'Christelle', 
+      fullName: 'Christelle Pelaya', 
+      image: '/assets/images/herve.png', 
+      alt: 'Christelle Pelaya', 
+      department: 'Dermatology', 
+      viewBtn: 'View', 
+      editBtn: 'Edit'
+    },
+    {
+      preferedName: 'Herve', 
+      fullName: 'Herve Mashukane', 
+      image: '/assets/images/herve.png', 
+      alt: 'Herve Mashukane', 
+      department: 'Cardiology', 
+      viewBtn: 'View', 
+      editBtn: 'Edit'
+    },
+    {
+      preferedName: 'Hiro', 
+      fullName: 'Hiro Mataba', 
+      image: '/assets/images/herve.png', 
+      alt: 'Hiro Mataba', 
+      department: 'Dermatology', 
+      viewBtn: 'View', 
+      editBtn: 'Edit'
+    },
+    {
+      preferedName: 'Patricia', 
+      fullName: 'Patricia Masiri', 
+      image: '/assets/images/herve.png', 
+      alt: 'Patricia Masiri', 
+      department: 'Neurology', 
+      viewBtn: 'View', 
+      editBtn: 'Edit'
+    },
+    {
+      preferedName: 'Herve', 
+      fullName: 'Herve Mashukane', 
+      image: '/assets/images/herve.png', 
+      alt: 'Herve Mashukane', 
+      department: 'Surgery', 
+      viewBtn: 'View', 
+      editBtn: 'Edit'
+    },
+    {
+      preferedName: 'Hiro', 
+      fullName: 'Hiro Mataba', 
+      image: '/assets/images/herve.png', 
+      alt: 'Hiro Mataba', 
+      department: 'Oncology', 
+      viewBtn: 'View', 
+      editBtn: 'Edit'
+    },
+  ]
 }
