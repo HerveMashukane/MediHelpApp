a90fe122e83eb7a0a145d6049f3e5adceaf8aa59
Herve <hervemashukane@gmail.com>
2025-09-05 10:47:07 +0100

    style medication tracker page

--- src/app/pages/medication-tracker/medication-tracker.component.ts
+++ src/app/pages/medication-tracker/medication-tracker.component.ts
@@ -1,12 +1,38 @@
 import { Component } from '@angular/core';
+import { CommonModule } from "@angular/common";
 
 @Component({
   selector: 'app-medication-tracker',
   standalone: true,
-  imports: [],
+  imports: [CommonModule],
   templateUrl: './medication-tracker.component.html',
   styleUrl: './medication-tracker.component.css'
 })
 export class MedicationTrackerComponent {
-
+  medicationsTracker: any = [
+  {
+    name: 'Amoxicillin', 
+    dosage: '500mg', 
+    schedule: 'Twice a day', 
+    status: 'Active', 
+    editBtn: 'Edit', 
+    deleteBtn: 'Delete'
+  },
+  {
+    name: 'Vitamin C', 
+    dosage: '1000mg', 
+    schedule: 'Once a day', 
+    status: 'Pending', 
+    editBtn: 'Edit', 
+    deleteBtn: 'Delete'
+  },
+  {
+    name: 'Ibuprofen', 
+    dosage: '200mg', 
+    schedule: 'Every 6hours', 
+    status: 'Active', 
+    editBtn: 'Edit', 
+    deleteBtn: 'Delete'
+  },
+  ]
 }
