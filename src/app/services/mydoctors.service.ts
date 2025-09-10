df823a4dff3180713ba03f4c4b8d727d85a4cb4f
Herve <hervemashukane@gmail.com>
2025-09-10 15:25:34 +0100

    check if platform is a browser before local storage implementation

--- src/app/services/mydoctors.service.ts
+++ src/app/services/mydoctors.service.ts
@@ -1,5 +1,6 @@
-import { Injectable } from '@angular/core';
+import { Injectable, Inject, PLATFORM_ID } from '@angular/core';
 import { BehaviorSubject } from 'rxjs';
+import { isPlatformBrowser } from '@angular/common';
 
 export interface Doctor {
   fullName: string,
@@ -12,11 +13,35 @@ export interface Doctor {
 })
 
 export class MydoctorsService {
+  private doctors: Doctor[] = [];
   private doctorsSource = new BehaviorSubject<Doctor[]>([]);
   doctors$ = this.doctorsSource.asObservable();
+
+  // check if platform is a browser
+  constructor(@Inject(PLATFORM_ID) platformId: object) {
+    this.doctors = isPlatformBrowser(platformId) ? this.loadDoctorsFromLocalStorage() : [];
+    this.doctorsSource?.next(this.doctors);
+  }  
+
+  // load doctors from localStorage
+  private loadDoctorsFromLocalStorage(): Doctor[] {
+    if (typeof localStorage !== 'undefined') {
+      const storedDoctors = localStorage.getItem('doctors');
+      return storedDoctors ? JSON.parse(storedDoctors) : [];
+    }
+    return [];
+  }
+
+  // doctors to localStorage
+  private saveDoctorsToLocalStorage(): void {
+    if (typeof localStorage !== 'undefined') {
+      localStorage.setItem('doctors', JSON.stringify(this.doctors));
+    }
+  }
 
   addDoctor(doctor: Doctor) {
     const currectDoctor = this.doctorsSource.value;
     this.doctorsSource.next([...currectDoctor, doctor]);
+    this.saveDoctorsToLocalStorage();
   }
 }
