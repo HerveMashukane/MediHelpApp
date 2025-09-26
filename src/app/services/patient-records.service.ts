eb7a433548e4d50d158a952ef2ab73411f7856cf
Herve <hervemashukane@gmail.com>
2025-09-26 16:34:47 +0100

    save to and load patients from local storage

--- src/app/services/patient-records.service.ts
+++ src/app/services/patient-records.service.ts
@@ -19,13 +19,26 @@ export interface Patient {
 })
 export class PatientRecordsService {
 
-  private patientsSource = new BehaviorSubject<Patient[]>([]);
+  // observable list of patients
+  private patientsSource = new BehaviorSubject<Patient[]>(this.loadPatientsFromLocalStorage());
   patients$ = this.patientsSource.asObservable();
+
+  // save patients to local storage
+  private savePatientsToLocalStorage(patients: Patient[]) {
+    localStorage.setItem('patients', JSON.stringify(patients));
+  }
+
+  // load patients from local storage
+  private loadPatientsFromLocalStorage() {
+    const storedPatients = localStorage.getItem('patients');
+    return storedPatients ? JSON.parse(storedPatients) : [];
+  }
 
   // add new patient
   addPatient(patient: Patient) {
     const currentPatients = this.patientsSource.value;
     const updatedPatients = [...currentPatients, patient];
     this.patientsSource.next(updatedPatients);
+    this.savePatientsToLocalStorage(updatedPatients);
   }
 }
