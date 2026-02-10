94858001fcec87c8132154368d4237f44d5ce601
Herve <hervemashukane@gmail.com>
2026-02-10 20:15:29 +0100

    design uppointments template

--- src/app/pages/appointments/appointments.component.ts
+++ src/app/pages/appointments/appointments.component.ts
@@ -1,14 +1,17 @@
 import { CommonModule } from '@angular/common';
 import { Component } from '@angular/core';
+import { AppointmentsFormComponent } from './appointments-form/appointments-form.component';
 
 @Component({
   selector: 'app-appointments',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, AppointmentsFormComponent],
   templateUrl: './appointments.component.html',
   styleUrl: './appointments.component.css'
 })
 export class AppointmentsComponent {
+  isFormClicked: boolean = false;
+
   patientAppointments: any = [
     {patientName: 'Herve Mashukane', doctorName: 'Christelle Pelaya', date: 'September 2, 2025', time: '10:00AM', status: 'Completed', viewBtn: 'View', editBtn: 'Edit', cancelBtn: 'Cancel'},
     {patientName: 'Hiro Mataba', doctorName: 'Herve Mashukane', date: 'August 10, 2025', time: '11:30AM', status: 'Pending', viewBtn: 'View', editBtn: 'Edit', cancelBtn: 'Cancel'},
@@ -23,4 +26,8 @@ export class AppointmentsComponent {
     {patientName: 'Hiro Mataba', doctorName: 'Herve Mashukane', date: 'February 18, 2025', status: 'Pending', time: '3:30PM', viewBtn: 'View', editBtn: 'Edit', cancelBtn: 'Cancel'},
     {patientName: 'Christelle Pelaya', doctorName: 'Hiro Mataba', date: 'January 15, 2025', status: 'Canceled', time: '4:00PM', viewBtn: 'View', editBtn: 'Edit', cancelBtn: 'Cancel'},
   ]
+
+  toggleForm() {
+    this.isFormClicked = !this.isFormClicked;
+  }
 }
