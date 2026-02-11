5e4ffd871cf5c51d482408b9e651ab05e81b3ef5
Herve <hervemashukane@gmail.com>
2026-02-11 11:55:42 +0100

    restyle appointments form

--- src/app/pages/appointments/appointments.component.ts
+++ src/app/pages/appointments/appointments.component.ts
@@ -12,17 +12,17 @@ export class AppointmentsComponent {
   isFormClicked: boolean = false;
 
   patientAppointments: any = [
-    {patientName: 'Herve Mashukane', doctorName: 'Christelle Pelaya', date: 'September 2, 2025', time: '10:00AM', status: 'Completed', viewBtn: 'View', editBtn: 'Edit', cancelBtn: 'Cancel'},
-    {patientName: 'Hiro Mataba', doctorName: 'Herve Mashukane', date: 'August 10, 2025', time: '11:30AM', status: 'Pending', viewBtn: 'View', editBtn: 'Edit', cancelBtn: 'Cancel'},
-    {patientName: 'Christelle Pelaya', doctorName: 'Hiro Mataba', date: 'July 2, 2025', time: '2:00PM', status: 'Canceled', viewBtn: 'View', editBtn: 'Edit', cancelBtn: 'Cancel'},
-    {patientName: 'Herve Mashukane', doctorName: 'Christelle Pelaya', date: 'March 22, 2025', time: '2:30PM', status: 'Completed', viewBtn: 'View', editBtn: 'Edit', cancelBtn: 'Cancel'},
-    {patientName: 'Hiro Mataba', doctorName: 'Herve Mashukane', date: 'February 18, 2025', status: 'Pending', time: '3:30PM', viewBtn: 'View', editBtn: 'Edit', cancelBtn: 'Cancel'},
-    {patientName: 'Christelle Pelaya', doctorName: 'Hiro Mataba', date: 'January 15, 2025', status: 'Canceled', time: '4:00PM', viewBtn: 'View', editBtn: 'Edit', cancelBtn: 'Cancel'},
-    {patientName: 'Herve Mashukane', doctorName: 'Christelle Pelaya', date: 'September 2, 2025', time: '10:00AM', status: 'Completed', viewBtn: 'View', editBtn: 'Edit', cancelBtn: 'Cancel'},
-    {patientName: 'Hiro Mataba', doctorName: 'Herve Mashukane', date: 'August 10, 2025', time: '11:30AM', status: 'Pending', viewBtn: 'View', editBtn: 'Edit', cancelBtn: 'Cancel'},
-    {patientName: 'Christelle Pelaya', doctorName: 'Hiro Mataba', date: 'July 2, 2025', time: '2:00PM', status: 'Canceled', viewBtn: 'View', editBtn: 'Edit', cancelBtn: 'Cancel'},
-    {patientName: 'Herve Mashukane', doctorName: 'Christelle Pelaya', date: 'March 22, 2025', time: '2:30PM', status: 'Completed', viewBtn: 'View', editBtn: 'Edit', cancelBtn: 'Cancel'},
-    {patientName: 'Hiro Mataba', doctorName: 'Herve Mashukane', date: 'February 18, 2025', status: 'Pending', time: '3:30PM', viewBtn: 'View', editBtn: 'Edit', cancelBtn: 'Cancel'},
-    {patientName: 'Christelle Pelaya', doctorName: 'Hiro Mataba', date: 'January 15, 2025', status: 'Canceled', time: '4:00PM', viewBtn: 'View', editBtn: 'Edit', cancelBtn: 'Cancel'},
+    {patientName: 'Herve Mashukane', doctorName: 'Christelle Pelaya', date: 'September 2, 2025', time: '10:00AM', status: 'Completed'},
+    {patientName: 'Hiro Mataba', doctorName: 'Herve Mashukane', date: 'August 10, 2025', time: '11:30AM', status: 'Pending'},
+    {patientName: 'Christelle Pelaya', doctorName: 'Hiro Mataba', date: 'July 2, 2025', time: '2:00PM', status: 'Canceled'},
+    {patientName: 'Herve Mashukane', doctorName: 'Christelle Pelaya', date: 'March 22, 2025', time: '2:30PM', status: 'Completed'},
+    {patientName: 'Hiro Mataba', doctorName: 'Herve Mashukane', date: 'February 18, 2025', status: 'Pending', time: '3:30PM'},
+    {patientName: 'Christelle Pelaya', doctorName: 'Hiro Mataba', date: 'January 15, 2025', status: 'Canceled', time: '4:00PM'},
+    {patientName: 'Herve Mashukane', doctorName: 'Christelle Pelaya', date: 'September 2, 2025', time: '10:00AM', status: 'Completed'},
+    {patientName: 'Hiro Mataba', doctorName: 'Herve Mashukane', date: 'August 10, 2025', time: '11:30AM', status: 'Pending'},
+    {patientName: 'Christelle Pelaya', doctorName: 'Hiro Mataba', date: 'July 2, 2025', time: '2:00PM', status: 'Canceled'},
+    {patientName: 'Herve Mashukane', doctorName: 'Christelle Pelaya', date: 'March 22, 2025', time: '2:30PM', status: 'Completed'},
+    {patientName: 'Hiro Mataba', doctorName: 'Herve Mashukane', date: 'February 18, 2025', status: 'Pending', time: '3:30PM'},
+    {patientName: 'Christelle Pelaya', doctorName: 'Hiro Mataba', date: 'January 15, 2025', status: 'Canceled', time: '4:00PM'},
   ]
 }
